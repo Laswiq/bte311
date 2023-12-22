b7cf6a181b2f12a18cb23da51a8f6cab26f8b452
Malik Kılıçarslan <84089939+maliklcrsln@users.noreply.github.com>
2023-12-22 11:32:39 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rastgele Sayı Tablosu</title>
+</head>
+<body>
+
+<form method="post" action="">
+  <table> 
+    <tr> 
+    <td><label for="satir">Satır Sayısı:</label></td>
+    <td><input type="number" name="satir" required></td>
+    </tr>
+    <tr>
+    <td><label for="sutun">Sütun Sayısı:</label></td>
+    <td><input type="number" name="sutun" required></td>
+    </tr>
+    <tr>
+    <td rowspan="2"><input type="submit" value="Tabloyu Oluştur"></td></tr>
+    </table>
+</form>
+
+<?php
+// Kullanıcının girdiği satır ve sütun sayılarını alın
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $satir_sayisi = isset($_POST['satir']) ? intval($_POST['satir']) : 0;
+    $sutun_sayisi = isset($_POST['sutun']) ? intval($_POST['sutun']) : 0;
+
+    // Girilen sayılar geçerliyse tabloyu oluştur
+    if ($satir_sayisi > 0 && $sutun_sayisi > 0) {
+        echo '<table border="1">';
+        for ($i = 1; $i <= $satir_sayisi; $i++) {
+            echo '<tr>';
+            for ($j = 1; $j <= $sutun_sayisi; $j++) {
+                $random_sayi = rand(1, 100);
+                echo '<td>' . $random_sayi . '</td>';
+            }
+            echo '</tr>';
+        }
+        echo '</table>';
+    } else {
+        echo 'Lütfen geçerli bir satır ve sütun sayısı girin.';
+    }
+}
+?>
+
+</body>
+</html>
---
+++ index1.php
@@ -0,0 +1,13 @@
+<html> 
+<body> 
+<?php 
+
+for ($i = 0; $i < 100; $i++) {
+	if ($i % 2 === 0) 
+		{ continue; } 
+	echo "$i\n<br>"; 
+}
+
+?> 
+</body> 
+</html>
